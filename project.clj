7f64662d337247f8d137e8b92bb1d331587dcd9d
Bart Brouns <bart@magnetophon.nl>
2016-08-12 11:45:22 +0200

    clean up

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [overtone "0.10.1"]
                  [quil "2.4.0"]
-                 [automat "0.2.0"]
+                 ;; [automat "0.2.0"]
                  [reduce-fsm "0.1.4"]]
   :main ^:skip-aot openloop.core
   :target-path "target/%s"
--- src/openloop/states.clj
+++ src/openloop/states.clj
@@ -1,49 +1,26 @@
 (ns openloop.states
-  (require [automat.viz :refer (view)]
-           [automat.core :as a])
-  )
+  (require
+   ;; [automat.viz :refer (view)]
+   ;; [automat.core :as a]
+   [reduce-fsm :as fsm]
+   ))
 
-(view [1 2 3 4])
-(view (a/or [1 2 3] [1 3]))
-(partial)
-(def f (a/compile
-        [1 2 3 (a/$ :complete)]
-        {:reducers {:complete (fn [state input] :completed)}})))
-(partial a/advance f)
-(def adv (partial a/advance f))
-(a/advance f nil 42 :error)
-(view f)
-
-(def pages [:cart :checkout :cart])
-(def page-pattern
-  (vec
-   (interpose (a/* a/any) pages)))
-(interpose (a/* a/any) pages)
-(page-pattern)
-(view page-pattern)
-(view (a/or [:sent :acknowledged :accepted] [:sent :rejected (a/$
-                                                             :not-accepted)]))
-(view (a/compile (a/or [:sent :acknowledged (a/or :accepted
-                                                  :rejected)]
-                       [:sent :malformed])))
-(def )
-(view (a/or [:loop-btn] [:]))o
-
-(require '[reduce-fsm :as fsm])
+(defn inc-val [val & _] (inc val))
 
 (fsm/defsm-inc loop-fsm
   [[:idle
     :loop-btn -> :rec-master
-    :tap -> :start-counting]
+    :tap -> {:action inc-val} :start-counting]
    [:rec-master
-    :loop-btn ->  :play-master]
+    :loop-btn ->  :play-master
+    :timeout -> :idle]
    [:start-counting
-    :tap -> :counting
+    :tap -> {:action inc-val} :counting
     :loop-btn -> :rec-master
     :timeout -> :idle
     ]
    [:counting
-    :tap -> :counting
+    :tap -> {:action inc-val} :counting
     :loop-btn -> :rec-master
     :timeout -> :rec-master
     ]
@@ -53,4 +30,36 @@
 
 (fsm/show-fsm loop-fsm)
 
+(def fsm-state (atom (loop-fsm  0)))
+(:value @fsm-state)
+(:state @fsm-state)
+
+(swap! fsm-state fsm/fsm-event :loop-btn)
+(swap! fsm-state fsm/fsm-event :tap)
 (loop-fsm 0)
+
+
+;; (view [1 2 3 4])
+;; (view (a/or [1 2 3] [1 3]))
+;; (partial)
+;; (def f (a/compile
+;;         [1 2 3 (a/$ :complete)]
+;;         {:reducers {:complete (fn [state input] :completed)}})))
+;; (partial a/advance f)
+;; (def adv (partial a/advance f))
+;; (a/advance f nil 42 :error)
+;; (view f)
+;; (def pages [:cart :checkout :cart])
+;; (def page-pattern
+;;   (vec
+;;    (interpose (a/* a/any) pages)))
+;; (interpose (a/* a/any) pages)
+;; (page-pattern)
+;; (view page-pattern)
+;; (view (a/or [:sent :acknowledged :accepted] [:sent :rejected (a/$
+;;                                                               :not-accepted)]))
+;; (view (a/compile (a/or [:sent :acknowledged (a/or :accepted
+;;                                                   :rejected)]
+;;                        [:sent :malformed])))
+;; (def )
+;; (view (a/or [:loop-btn] [:]))o
