672fa0c59ee02ac42cb06d7517159fb921d6e140
Bart Brouns <bart@magnetophon.nl>
2016-08-12 19:04:47 +0200

    move SR to init, so constants doesn't need overtone

--- src/openloop/constants.clj
+++ src/openloop/constants.clj
@@ -1,9 +1,6 @@
-(ns openloop.constants
-  (use [overtone.core]))
+(ns openloop.constants )
 
 ;; set some constants
-(def SR (:sample-rate (server-info)))
 (def max-loop-seconds 30)
-(def max-loop-samples (* max-loop-seconds SR))
 (def nr-chan 2)
 (def nr-loops 8)
