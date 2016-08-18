c1dd4b43e01f270b85112ecdb40485ced190c656
Bart Brouns <bart@magnetophon.nl>
2016-08-18 16:53:43 +0200

    original idea for undo state spec'd, rename defs

--- src/openloop/states.clj
+++ src/openloop/states.clj
@@ -65,11 +65,11 @@ openloop.constants
 (pprint (s/exercise ::all-loops 2))
 
 (s/def ::loop-type (s/cat
-                    :length pos-int?
+                    :length ::loop-length-type
                     :audio ::loop-audio-type
                     :FX ::FX-type
                     :osc ::loop-osc-type
-                    :prev-sources ::prev-sources-type ))
+                    :modification-stack ::modification-stack-type ))
 
 (s/def ::loop-audio-type (s/cat :dry-audio ::audio-type
                                 :wet-audio ::audio-type))
@@ -78,57 +78,53 @@ openloop.constants
 
 ;; max loop length is just a few samples while designing the data structures
 (def max-loop-length 8)
+(def max-undo-nr 10)
 (s/def ::audio-type (s/coll-of ::mono-audio-type :kind vector? :count nr-chan))
 (s/def ::mono-audio-type (s/every double? :kind vector? :count max-loop-length ))
 ;; (s/def ::mono-audio-type double-array?)
 
 (s/def ::FX-type int?)
 (s/def ::loop-osc-type int?)
-(s/def ::prev-sources-type (s/cat
-                            :audio ::prev-audio-type
-                            :osc ::prev-osc-type))
-(s/def ::kws (s/with-gen (s/and keyword? #(= (namespace %) "my.domain"))
-               #(s/gen #{:my.domain/name :my.domain/occupation :my.domain/id})))
-(s/valid? ::kws :my.domain/name)  ;; true
-(gen/sample (s/gen ::kws))
+(s/def ::modification-stack-type (s/cat
+                                  :audio ::audio-modification-stack-type
+                                  :osc ::osc-modification-stack-type))
 
-(s/def ::prev-audio-indexes-type (s/with-gen
-                                   (s/coll-of ::loop-index-type :min-count 0, :max-count max-loop-length :distinct true :into #{} )
-                                   #(gen/sorted-set (s/gen ::loop-index-type) ) ))
+;; (s/def ::prev-audio-indexes-type (s/with-gen
 
-(pprint (gen/sample (s/gen ::prev-audio-indexes-type) 20))
+;; (pprint (gen/sample (s/gen ::prev-audio-indexes-type) 20))
 
-(s/def ::prev-audio-type (s/coll-of ::audio-block-type, :count (count ), :max-count max-loop-length :distinct true :into #{} ))
-(s/def ::prev-audio-type (s/* ::audio-block-type))
-(s/int-in-range? 0 8 7)
+;; (s/def ::audio-modification-stack-type  (s/coll-of ::audio-modification-type, :count (count ), :max-count max-loop-length :distinct true :into #{} ))
+;; (s/def ::audio-modification-stack-type  (s/* ::audio-modification-type))
 
-(s/def ::audio-block-type (s/cat
-                           :start-in-loop ::prev-audio-indexes-type
-                           :length ::loop-length-type
-                           :start-source pos-int?))
+(s/def ::audio-modification-type (s/cat ; a modification to the audio in a loop buffer
+                                  :dest-index ::loop-index-type ; where does the new audio go?
+                                  :length ::loop-length-type ; how many samples do we replace?
+                                  :src-index pos-int?)) ; where does the new audio come from?
+
+;; (s/def ::audio-modification-stack-type  (s/map-of pos-int? ::audio-modification-type))
+(s/def ::audio-modification-stack-type (s/coll-of ::audio-modification-type, :max-count max-undo-nr ))
+
+(println "oooooooooooooooooooooooooooo")
+(pprint (gen/generate (s/gen ::audio-modification-stack-type  )))
+(pprint (s/exercise ::loop-type  2))
+(s/def ::int-map (s/map-of keyword? integer?))
+(gen/sample (s/gen ::int-map))
+(gen/map)
 
 (s/def ::loop-index-type (s/int-in 0 max-loop-length))
 ;; (s/def ::loop-index-type (s/spec #(s/int-in-range? 0 max-loop-length %)))
 (s/def ::loop-length-type (s/int-in 1 (inc  max-loop-length)))
 
-(println "oooooooooooooooooooooooooooo")
-(pprint (s/exercise ::prev-audio-type max-loop-length))
-(s/describe ::prev-audio-type)
+(pprint (s/exercise ::audio-modification-stack-type  max-loop-length))
+(s/describe ::audio-modification-stack-type )
 (pprint (s/exercise ::loop-length-type 20))
-(pprint (s/exercise ::audio-block-type max-loop-length))
-(pprint (s/exercise ::prev-audio-indexes-type 40))
-(apply sorted-set #{0 1 4 6 2 8})
-(s/explain ::prev-audio-type [{:start-in-loop 3, :length 7, :start-source 1}
-                              {:start-in-loop 0, :length 2, :start-source 4}
-                              {:start-in-loop 2, :length 2, :start-source 12}] )
-(s/explain ::loop-index-type 2)
+;; (pprint (s/exercise ::prev-audio-indexes-type 40))
 
-(s/def ::prev-osc-type int? )
+(s/def ::osc-modification-stack-type int? )
 
 (pprint (s/exercise ::loop-type 4))
 
 
-(gen/generate (s/gen ::prev-audio-indexes-type ))
 
 (def email-regex #"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$")
 (s/def ::email-type (s/and string? #(re-matches email-regex %)))
@@ -183,10 +179,11 @@ openloop.constants
 (def loop-FX nil)
 (def loop-osc nil)
 (def prev-sources-array {:audio source-audio-array :osc loop-osc-array}) ; a list of mutations to the audio and osc
-(def source-audio-array [source-audio-block]) ; a list of mutations to the loop audio buffer
+(def source-audio-array [source-audio-block]) ; a chronological vector  of mutations to the loop audio buffer
 (def source-audio-block ; a block of source audio mapped to a block of buffer audio
-  {:length 0 ; how long is the block of audio that we want to replace
+  {
    :start-loop 0 ; where does it go in the buffer
+   :length 0 ; how long is the block of audio that we want to replace
    :start-source 0 ; where does it come from
    :xfade-data x-fade; how should we x-fade from the previous audio to the new and back
    })
@@ -195,7 +192,7 @@ openloop.constants
    :xfade-offset 0 ;where should the center of the x-fade be, relative to the start position
                                         ; possibly other data, like shape
    })
-(def loop-osc-array [loop-osc-block] ) ; a list of mutations
+(def loop-osc-array [loop-osc-block] ) ; a chronological vector of mutations
 (def loop-osc-block ; a block of source audio mapped to a block of buffer audio
   {:length 0 ; how long is the block of audio that we want to replace
    :start-loop 0 ; where does it go in the buffer
