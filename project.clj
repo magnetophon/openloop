3c42a5555a74472c7a286dc849590a2c8c7a049a
Bart Brouns <bart@magnetophon.nl>
2016-08-19 20:44:24 +0200

    put clojure/test.check in :dev profile

--- project.clj
+++ project.clj
@@ -6,11 +6,11 @@
   :dependencies [
                  ;; [org.clojure/clojure "1.8.0"]
                  [org.clojure/clojure "1.9.0-alpha10"]
-                 [org.clojure/test.check "0.9.0"]
+                 ;; [org.clojure/test.check "0.9.0"]
                  [overtone "0.10.1"]
                  [quil "2.4.0"]
                  ;; [automat "0.2.0"]
                  [reduce-fsm "0.1.4"]]
   :main ^:skip-aot openloop.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all} :dev {:dependencies [[org.clojure/test.check "0.9.0"]]}})
