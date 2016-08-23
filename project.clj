e916e2e1f2c598a0c3b09091c820de1ffd18d902
Bart Brouns <bart@magnetophon.nl>
2016-08-23 18:31:37 +0200

    use overtone from git, to be compatible with clojure 1.9.0 beta

--- project.clj
+++ project.clj
@@ -7,10 +7,28 @@
                  ;; [org.clojure/clojure "1.8.0"]
                  [org.clojure/clojure "1.9.0-alpha10"]
                  ;; [org.clojure/test.check "0.9.0"]
-                 [overtone "0.10.1"]
+                 ;; [overtone "0.10.1"]
                  [quil "2.4.0"]
                  ;; [automat "0.2.0"]
-                 [reduce-fsm "0.1.4"]]
+                 [reduce-fsm "0.1.4"]
+
+                 ;; for overtone:
+                 ;; [org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.json "0.2.3"]
+                 [clj-native "0.9.3"]
+                 [overtone/at-at "1.2.0"]
+                 [overtone/osc-clj "0.9.0"]
+                 [overtone/byte-spec "0.3.1"]
+                 [overtone/midi-clj "0.5.0"]
+                 [overtone/libs.handlers "0.2.0"]
+                 [overtone/scsynth "3.5.7.0"]
+                 [overtone/scsynth-extras "3.5.7.0"]
+                 [clj-glob "1.0.0"]
+
+                 ]
+  :plugins [[lein-git-deps "0.0.1-SNAPSHOT"]]
+  :git-dependencies [["https://github.com/overtone/overtone.git"]]
+  :source-paths ["src" ".lein-git-deps/overtone/src/"]
   :main ^:skip-aot openloop.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all} :dev {:dependencies [[org.clojure/test.check "0.9.0"]]}})
