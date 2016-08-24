698c8919f7fe3efccd4678f55dae05b9544087ca
Bart Brouns <bart@magnetophon.nl>
2016-08-24 12:57:15 +0200

    re-order def of specs from small to big

--- src/openloop/spec.clj
+++ src/openloop/spec.clj
@@ -10,57 +10,21 @@
 ;; spec  input state
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(s/def ::inputs
+(s/def ::inputs-type
   (s/cat
-   :loop-btn-downs nil
-   :tap nil
-   :timeout nil ))
+   :loop-btn-downs (s/coll-of boolean? :count nr-loops)
+   :tap boolean?
+   :timeout boolean? ))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; spec  loop data
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; a-loop has everything we need to:
-;;    - play a loop
-;;    - change a loop
-;;    - reconstruct it from dry signal
-(s/def ::loop-type (s/cat
-                    ;; :loop-nr ::buffer-nr-type ; which of the nr-loops buffers are we?    redundant, cause we store this in
-                    :length ::loop-length-type
-                    :sources ::sources-type  ; for each point in the buffer, define where the data came from
-                    ;; we don't keep audio data in our state, it will be managed by scsynth
-                    ;; :audio ::loop-audio-type ; the actual audio, dry and wet (effected)
-                    :FX ::FX-type ; the effect chain that gets dry to wet
-                    ;; :osc ::loop-osc-type ; the automation for the FX ; will be derived from the sources
-                    ))
+(s/def ::loop-index-type (s/int-in 0 max-loop-length))
+(s/def ::loop-length-type (s/int-in 1 (inc  max-loop-length)))
 
-;; (s/def ::loop-audio-type (s/cat :dry-audio ::buffer-nr-type
-;;                                 :wet-audio ::buffer-nr-type))
-;; (s/def ::buffer-nr-type (s/int-in 0 (* 2 nr-loops)))
-;; (s/def ::loop-audio-type (s/cat :dry-audio ::audio-type ; the source audio, needed to construct:
-;;                                 :wet-audio ::audio-type)) ; the effected audio
-;; todo: exclude NAN and Infinity.
-;; todo: see below
-;; from https://github.com/candera/dynne
-;; Thanks to Rich Hickey for his suggestion to rewrite dynne in terms of sequences of double arrays, which led to a massive increase in performance.
-;; (s/def ::mono-audio-type double-array?) ; doesn't work
-;; (s/def ::audio-type (s/coll-of ::mono-audio-type :count nr-chan))
-;; (s/def ::mono-audio-type (s/every double? :count max-loop-length ))
-(println "000000000000000000000000000000000")
-;; (pprint (s/exercise ::FX-type 4))
-
-
-(s/def ::FX-type (s/cat; the FX chain needed to create the wet audio
-                  :internal pos-int? ; scsynth internal FX
-                  :external (s/cat
-                             :jack-name string? ; the jack name to check if it's running
-                             :command string? ; the cli command that runs the FX
-                             :port pos-int? ; the OSC port to send automation to
-                             )))
-;; (s/def ::loop-osc-type pos-int?) ; placeholder for the osc data of a loop
-(s/def ::sources-type (s/cat
-                       :audio ::audio-sources-type ; where did the dry audio come from
-                       :osc ::osc-sources-type)) ; which combination of OSC input streams and calculations lead to the current automation of this loop
+(s/def ::x-fade-data-type (s/cat
+                           :length (s/int-in 0 max-x-fade-length))) ; for now, just indicate how long the crossfade is in ms
 
 (s/def ::audio-block-type (s/cat  ; a block of source audio mapped to a block of buffer audio
                            ;; :dest-index ::loop-index-type ; where does the new audio go?  ; superfluous, since we store this object in a map with dest-indexes as keys
@@ -70,10 +34,6 @@
                            :x-fade-data ::x-fade-data-type ; how do we crossfade from one to the next?
                            ))
 
-(s/def ::x-fade-data-type (s/cat
-                           :length (s/int-in 0 max-x-fade-length))) ; for now, just indicate how long the crossfade is in ms
-;; maybe needed for performance, so it's easier to know which nodes to replace or change on updates.
-;; (s/def ::audio-dest-indexes-type (s/coll-of ::loop-index-type, :min-count 0, :max-count max-loop-length :distinct true :into #{} ))
 
 ;; the keys represent the start index in the loop where the block should go.
 (s/def ::audio-sources-type
@@ -83,11 +43,13 @@
       (fn [m] (into (sorted-map) m))
       (gen/map (s/gen ::loop-index-type ) (s/gen ::audio-block-type )))))
 
-(println "ooooooooooooooooo")
-(pprint (drop 90 (s/exercise ::audio-sources-type 91)))
+;; (println "ooooooooooooooooo")
+;; (pprint (drop 90 (s/exercise ::audio-sources-type 91)))
 
-(s/def ::loop-index-type (s/int-in 0 max-loop-length))
-(s/def ::loop-length-type (s/int-in 1 (inc  max-loop-length)))
+(s/def ::osc-params-type pos-int?) ; placeholder
+(s/def ::osc-source-type pos-int?) ; placeholder
+(s/def ::osc-function-type (s/with-gen (s/and keyword? #(= (namespace %) "openloop.osc_functions"))
+                             #(s/gen #{:openloop.osc_functions/sum :openloop.osc_functions/example :openloop.osc_functions/filter})))
 
 (s/def ::osc-sources-type
   (s/cat
@@ -99,12 +61,6 @@
                                         ; optional function that combines the above sources into the output
    :function ::osc-function-type
    ))
-
-(s/def ::osc-params-type pos-int?) ; placeholder
-(s/def ::osc-source-type pos-int?) ; placeholder
-
-(s/def ::osc-function-type (s/with-gen (s/and keyword? #(= (namespace %) "openloop.osc_functions"))
-                             #(s/gen #{:openloop.osc_functions/sum :openloop.osc_functions/example :openloop.osc_functions/filter})))
 
 
 ;; (s/def ::osc-function-type (s/with-gen (s/and keyword? #(= (namespace %) "openloop.osc_functions"))
@@ -123,8 +79,36 @@
 ;; (gen/sample osc-function-gen 5)
 
 
+(s/def ::sources-type (s/cat
+                       :audio ::audio-sources-type ; where did the dry audio come from
+                       :osc ::osc-sources-type)) ; which combination of OSC input streams and calculations lead to the current automation of this loop
+
+(s/def ::FX-type (s/cat; the FX chain needed to create the wet audio
+                  :internal pos-int? ; scsynth internal FX
+                  :external (s/cat
+                             :jack-name string? ; the jack name to check if it's running
+                             :command string? ; the cli command that runs the FX
+                             :port pos-int? ; the OSC port to send automation to
+                             )))
+
+;; a loop has everything we need to:
+;;    - play a loop
+;;    - change a loop
+;;    - reconstruct the wet from the dry signal
+(s/def ::loop-type (s/cat
+                    ;; :loop-nr ::buffer-nr-type ; which of the nr-loops buffers are we?    redundant, cause we store this in
+                    :length ::loop-length-type
+                    :sources ::sources-type  ; for each point in the buffer, define where the data came from
+                    ;; we don't keep audio data in our state, it will be managed by scsynth
+                    ;; :audio ::loop-audio-type ; the actual audio, dry and wet (effected)
+                    :FX ::FX-type ; the effect chain that gets dry to wet
+                    ;; :osc ::loop-osc-type ; the automation for the FX ; will be derived from the sources
+                    ))
+;; (s/def ::loop-osc-type pos-int?) ; placeholder for the osc data of a loop
+
 (s/def ::all-loops-type  ; a vector of all loops contributing to the output.
   (s/coll-of ::loop-type :max-count nr-loops))
+
 
 (s/def ::current-state-type ; the overall state of the program
   (s/cat
@@ -146,6 +130,7 @@
    :booted boolean? ; is scsynth booted
    :connected boolean? ; is scsynth connected
    :saved-state ::saved-state-type ; everything we need for undo/redo and saving projects
+   :inputs ::inputs-type
    ))
 
 
